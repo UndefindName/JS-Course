992dc3a4cad593ccc2f752bc9945580fb5e6ea42
UndefindName <abubencov12@gmail.com>
2020-12-29 23:00:38 +0300

    Пройдено до 1.5

---
+++ Основы JS/1.4_Variables/index.js
@@ -0,0 +1,31 @@
+let userName = 'John';
+let age = 25;
+let message = 'Hello!';
+
+let user = [userName, age, message];
+
+alert(user);
+user.forEach(alert);
+
+/*
+ const NewYear = 31.12;
+ NewYear = 5;
+ alert(NewYear)
+ Вот здесь ошибка и она не отображается в редакторе.
+*/
+
+// Задание №1
+
+let admin;
+let name = 'Джон';
+
+admin = name;
+
+alert(admin)
+
+// Задание №2
+const ourPlanetName = 'Земля';
+let currentUserName;
+// Задание №3
+const BIRTHDAY = '30.03';
+const age = 16
---
+++ Основы JS/1.5_Data_Types/index.js
@@ -0,0 +1,66 @@
+// Теория
+let message = 'hello';
+message = 123456;
+alert(message)
+
+// Числовые
+let n = 123;
+n = 12.345;
+alert(n);
+
+alert(1/0);
+
+// BigInt
+const BigInt = 3578904657654374586785496518975165n;
+alert(BigInt);
+
+// Строка
+let str = "Привет";
+let str2 = 'Привет';
+let phrase = `Встраимоего выражения`;
+ alert(`Проверка, ${phrase}`)
+
+// Boolean
+let nameFieldChecke = true;
+let ageFieldChecked = false;
+
+let isGreater = 4 > 1;
+alert(isGreater);
+
+// null
+let age = null;
+
+// undefined
+let size = 123;
+alert(size);
+
+size = undefined;
+alert(size);
+
+// typeof
+alert(typeof undefined);
+
+alert(typeof o);
+
+alert(typeof 10n);
+
+alert(typeof true);
+
+alert(typeof "foo");
+
+alert(typeof Symbol("id"));
+
+alert(typeof Math);
+
+alert(typeof null);
+
+alert(typeof alert);
+
+// Задание
+let name = "Ilya";
+
+alert(`hello ${1}`);
+
+alert(`hello ${"name"}`);
+
+alert(`hello ${name}`);
