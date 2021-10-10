5e27038e3d4a6a4bf4cf0263f83abd753eb374f3
Qwerty <abubencov12@gmail.com>
2021-10-10 11:25:48 +0300

    WeakMap\Set

---
+++ Типы данных/8_WeakMap и WeakSet/index.js
@@ -0,0 +1,16 @@
+// WeakMap
+console.group('WeakMap');
+
+const weakMap = new WeakMap();
+
+let obj = {}
+
+weakMap.set(obj, "ok");
+console.log(weakMap)
+
+obj = null;
+console.log(weakMap)
+
+console.log('')
+console.groupEnd();
+
