da6f10d0455e57b21c2d9963590c4daa4eb5fb6d
UndefindName <abubencov12@gmail.com>
2022-12-30 18:52:24 +0300

    Разобрал 2ю главу

--- 09. Промисы, async_await/02. Промисы/index.js
+++ 09. Промисы, async_await/02. Промисы/index.js
@@ -1,44 +1,45 @@
 console.log('Промисы');
 
 let promise = new Promise(function (resolve, reject) {
-    if (Math.random() >= 0.5) resolve('Done!');
-    else reject(new Error('Whoops!'))
+    resolve('Done!');
 });
 
 promise.then(
-    result => console.log(result),
-).catch(
-    error => console.log(error),
+    function (result) {
+        console.log(result)
+
+        console.log('');
+        console.log('Пример: loadScript');
+
+        function loadScript(src) {
+            return new Promise(function (resolve, reject) {
+                let script = document.createElement('script');
+                script.src = src;
+
+                script.onload = () => resolve(script);
+                script.onerror = () => reject(new Error(`Ошибка загрузки скрипта ${src}`));
+
+                document.body.append(script)
+            })
+        }
+
+        let promScript = loadScript('script.js')
+
+        promScript.then(
+            script => {
+                console.log(script)
+
+                console.log('')
+                console.log('Задача №2');
+
+                function delay(ms) {
+                    return new Promise(resolve => setTimeout(resolve, ms));
+                };
+
+                delay(5).then(() => console.log('Выполнилось через 0,005 секунду'));
+            },
+            error => console.log(`Ошибка: ${error.message}`),
+        )
+
+    }
 )
-
-console.log('');
-// console.log('Пример: loadScript');
-
-// function loadScript(src) {
-//     let promise = new Promise(function(resolve, reject) {
-//         let script = document.createElement('script');
-//         script.src = src;
-
-//         script.onload = () => resolve(script);
-//         script.onerror = () => reject(new Error(`Ошибка загрузки скрипта ${src}`));
-
-//         document.body.append(script)
-//     })
-
-//     promise.then(
-//         script => console.log(script),
-//         error => console.log(`Ощибка: ${error.message}`),
-//     )
-
-//     return promise
-// }
-
-// let promScript = loadScript('script.js')
-
-console.log('Задача №2');
-
-// function delay(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// };
-
-// delay(1000).then(() => console.log('Выполнилось через 1 секунду'));
--- 09. Промисы, async_await/02. Промисы/script.js
+++ 09. Промисы, async_await/02. Промисы/script.js
@@ -1,1 +1,28 @@
-console.log('HEllO, WORLD!')
+function showCircle(cx, cy, radius) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div)
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            div.style.width = radius * 2 + 'px';
+            div.style.height = radius * 2 + 'px';
+
+            div.addEventListener('transitionend', function handler() {
+                div.removeEventListener('transitionend', handler);
+                resolve(div)
+            })
+        })
+    })
+}
+
+function go() {
+    showCircle(150, 150, 100).then(div => {
+        div.classList.add('message-ball');
+        div.append("Hello, World!")
+    })
+}
