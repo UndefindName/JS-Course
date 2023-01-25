991f70aa9615c8d4258942278821fdcea96bfdca
UndefindName <abubencov12@gmail.com>
2023-01-25 15:11:12 +0300

    Доделать

--- 02. Браузер/01. Документ/07. Изменение документа/index.js
+++ 02. Браузер/01. Документ/07. Изменение документа/index.js
@@ -1,1 +1,24 @@
-console.log('Изменение документа')
+console.log('Изменение документа');
+
+function createDIV() {
+    let arr = document.querySelectorAll('.alert')
+    let div = document.createElement('div');
+    let html = document.getElementById('divInput')
+
+
+    if(arr.length == 7) arr[6].remove()
+
+    console.log(arr.length)
+
+    div.className = 'alert';
+    div.innerHTML = `${html.value}`;
+
+    html.value = ''
+    document.body.children[1].prepend(div)
+}
+
+function check(event) {
+    if (event.which == 13) createDIV()
+}
+
+console.log
